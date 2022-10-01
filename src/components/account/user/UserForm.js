bf5b1762a28604f673d3d976138e6ee2c56785b4
issdilshod <dilshod@themembers.com>
2022-10-01 18:46:30 +0300

    minor fix

--- src/components/account/user/UserForm.js
+++ src/components/account/user/UserForm.js
@@ -9,7 +9,7 @@ const UserForm = () => {
         <div className='c-form'>
             <div className='c-form-head d-flex'>
                 <div className='mr-auto'>User</div>
-                <div class='c-times' onClick={ () => { triggerModalHide() } }>
+                <div className='c-times' onClick={ () => { triggerModalHide() } }>
                     <i><FaTimes /></i>
                 </div>
             </div>
